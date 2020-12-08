5f491e95d6713472052474b49e0363de00f59bed
Flemming Andersen <flemming@flamingcode.com>
2020-12-09 00:34:49 +0100

    Part 2 finished

--- day7/main.go
+++ day7/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -25,6 +26,10 @@ func main() {
 	answer := solvePuzzle1(m)
 
 	fmt.Printf("Puzzle 1 answer: %d\n", answer)
+
+	answer = solvePuzzle2(m)
+
+	fmt.Printf("Puzzle 2 answer: %d\n", answer)
 }
 
 // How many bag colors can eventually contain at least one shiny gold bag?
@@ -33,18 +38,42 @@ func solvePuzzle1(m bagMap) int {
 	return len(bags)
 }
 
-type bagMap map[string][]string
+func solvePuzzle2(m bagMap) int {
+	return m.countBags("shiny gold")
+}
+
+type bagMap map[string][]bag
+
+type bag struct {
+	count int
+	color string
+}
+
+func (m bagMap) countBags(search string) int {
+	var recursion func(key string, bags []bag) int
+	recursion = func(key string, bags []bag) int {
+		var count int
+		for _, b := range bags {
+			count += b.count + b.count*recursion(b.color, m[b.color])
+		}
+		return count
+	}
+	if _, ok := m[search]; !ok {
+		return 0
+	}
+	return recursion(search, m[search])
+}
 
 func (m bagMap) findBagsContaining(search string) []string {
 	found := make(map[string]struct{})
-	var recursion func(key string, colors []string)
-	recursion = func(key string, colors []string) {
-		for _, c := range colors {
-			if c == search {
+	var recursion func(key string, bags []bag)
+	recursion = func(key string, bags []bag) {
+		for _, b := range bags {
+			if b.color == search {
 				found[key] = struct{}{}
 			}
-			if len(m[c]) > 0 {
-				recursion(key, m[c])
+			if len(m[b.color]) > 0 {
+				recursion(key, m[b.color])
 			}
 		}
 	}
@@ -67,8 +96,8 @@ func readInput(r io.Reader) (bagMap, error) {
 		if l == "" {
 			continue
 		}
-		color, colors := splitLine(l)
-		m[color] = colors
+		color, bags := splitLine(l)
+		m[color] = bags
 	}
 	if err := s.Err(); err != nil {
 		return nil, err
@@ -76,15 +105,20 @@ func readInput(r io.Reader) (bagMap, error) {
 	return m, nil
 }
 
-var containRegex = regexp.MustCompile(`(?:\d+ ([^,]+) bags?(?:, )?)+?`)
+var containRegex = regexp.MustCompile(`(?:(\d+) ([^,]+) bags?(?:, )?)+?`)
 
-func splitLine(l string) (string, []string) {
+func splitLine(l string) (string, []bag) {
 	tmp := strings.SplitN(l, "contain", 2)
 	color := strings.TrimSuffix(strings.TrimSpace(tmp[0]), " bags")
 	matches := containRegex.FindAllStringSubmatch(strings.TrimSpace(tmp[1]), -1)
-	var colors []string
+	var bags []bag
 	for i := range matches {
-		colors = append(colors, matches[i][1])
+		count, _ := strconv.Atoi(matches[i][1])
+		bags = append(bags, bag{
+			count: count,
+			color: matches[i][2],
+		})
+
 	}
-	return color, colors
+	return color, bags
 }
--- day7/main_test.go
+++ day7/main_test.go
@@ -37,7 +37,7 @@ A light red bag, which can hold bright white and muted yellow bags, either of wh
 So, in this example, the number of bag colors that can eventually contain at least one shiny gold bag is 4.
 */
 
-func TestFind(t *testing.T) {
+func TestFindBagsContaining(t *testing.T) {
 	input := `light red bags contain 1 bright white bag, 2 muted yellow bags.
 	dark orange bags contain 3 bright white bags, 4 muted yellow bags.
 	bright white bags contain 1 shiny gold bag.
@@ -80,4 +80,68 @@ func TestFind(t *testing.T) {
 			t.Errorf("expected to find bag %q in list of valid bags", b)
 		}
 	}
+}
+
+func TestSolvePuzzle1(t *testing.T) {
+	input := `light red bags contain 1 bright white bag, 2 muted yellow bags.
+	dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+	bright white bags contain 1 shiny gold bag.
+	muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+	shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+	dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+	vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+	faded blue bags contain no other bags.
+	dotted black bags contain no other bags.`
+
+	m, err := readInput(strings.NewReader(input))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	answer := solvePuzzle1(m)
+
+	if answer != 4 {
+		t.Fatalf("expected answer 4, got %d", answer)
+	}
+}
+
+func TestSolvePuzzle2(t *testing.T) {
+	cases := []struct {
+		input  string
+		answer int
+	}{
+		{`light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.`, 32},
+
+		{`shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`, 126},
+	}
+
+	for _, tc := range cases {
+		t.Run("", func(t *testing.T) {
+			m, err := readInput(strings.NewReader(tc.input))
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			answer := solvePuzzle2(m)
+
+			if answer != tc.answer {
+				t.Fatalf("expected answer %d, got %d", tc.answer, answer)
+			}
+		})
+	}
+
 }
